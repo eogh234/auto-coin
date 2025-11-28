0d75fab185ceef3185d4943bb187fa7366b396d0
Daeho <eogh234@naver.com>
2025-11-29 00:51:48 +0900

    Fix modules/__init__.py - remove archived modules

--- modules/__init__.py
+++ modules/__init__.py
@@ -6,15 +6,11 @@ from .config_manager import ConfigManager
 from .notification_manager import NotificationManager
 from .learning_system import LearningSystem, TradeRecord
 from .trading_engine import TradingEngine
-from .backtest_engine import BacktestEngine
-from .performance_analyzer import PerformanceAnalyzer
 
 __all__ = [
     'ConfigManager',
     'NotificationManager',
     'LearningSystem',
     'TradeRecord',
-    'TradingEngine',
-    'BacktestEngine',
-    'PerformanceAnalyzer'
+    'TradingEngine'
 ]
